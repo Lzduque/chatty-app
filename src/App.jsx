76a1ea246b106aee55a4f1da5697278af5fefc78
Lzduque <lzduque@hotmail.com>
2019-05-07 21:32:45 +0000

    connecting to the server

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import ChatBar from './ChatBar.jsx';
 class App extends Component {
   constructor(props) {
     super(props);
-    // this is the *only* time you should assign directly to state:
+    this.socket = new WebSocket('ws://localhost:3001/');
     this.state = {
       currentUser: {name: "Bob"}, // optional. if currentUser is not defined, it means the user is Anonymous
       messages: [
@@ -27,15 +27,9 @@ class App extends Component {
 
   componentDidMount() {
     console.log("componentDidMount <App />");
-    setTimeout(() => {
-      console.log("Simulating incoming message");
-      // Add a new message to the list of messages in the data store
-      const newMessage = {id: 3, username: "Michelle", content: "Hello there!"};
-      const messages = this.state.messages.concat(newMessage)
-      // Update the state of the app component.
-      // Calling setState will trigger a call to render() in App and all child components.
-      this.setState({messages: messages})
-    }, 3000);
+    this.socket.onopen = function (event) {
+      console.log('Connected to server');
+    };
   }
 
   addMessage = (evt) => {
@@ -49,11 +43,20 @@ class App extends Component {
           content: evt.target.value
         }
       ];
-    this.setState({ messages: newMessages });
-    evt.target.value = '';
-    console.log("oldMessages: ", oldMessages);
-    console.log("newMessages: ", newMessages);
-    console.log("evt.target.value: ", evt.target.value);
+      this.setState({ messages: newMessages });
+
+      let msg = {
+        username: this.state.currentUser.name,
+        content: evt.target.value
+      };
+      // Send the msg object as a JSON-formatted string.
+      this.socket.send(JSON.stringify(msg));
+      console.log("msg: ", msg);
+
+      evt.target.value = '';
+      console.log("oldMessages: ", oldMessages);
+      console.log("newMessages: ", newMessages);
+      console.log("evt.target.value: ", evt.target.value);
     }
   }
 
